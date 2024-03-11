eecba5be85479a959924116f9c8b3cbc11bb000a
Chris Hill <1998630+bluedoorsoftware@users.noreply.github.com>
2024-03-11 17:42:18 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,10 @@ email_bittensor_start() {
   curl -fsSL https://raw.githubusercontent.com/bluedoorsoftware/reg-basher/main/emailer.sh | /bin/bash -s "da7WLNkctCdJYCkTnRAstg" "no-reply@bluedoorsoftware.co.uk" "chris@bluedoorsoftware.co.uk" "Test Subject: Bittensor Starting" "Test Content"
 }
 
+email_bittensor_start_with_hostname() { 
+  curl -fsSL https://raw.githubusercontent.com/bluedoorsoftware/reg-basher/main/emailer.sh | /bin/bash -s "da7WLNkctCdJYCkTnRAstg" "no-reply@bluedoorsoftware.co.uk" "chris@bluedoorsoftware.co.uk" "Test Subject: Bittensor Starting" "Test Content"
+}
+
 do_get_hostname() { 
   return curl -s http://169.254.169.254/metadata/v1.json | jq -r .hostname
 }
